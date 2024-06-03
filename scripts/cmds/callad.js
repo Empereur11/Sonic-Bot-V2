4eea48246e8f31b9b820a5919152a3cfdcb02bfd
Sonic-Daniel <danielthiam54@gmail.com>
2024-06-03 03:16:54 +0100

    Update callad.js

--- scripts/cmds/callad.js
+++ scripts/cmds/callad.js
@@ -56,9 +56,9 @@ module.exports = {
 		if (config.adminBot.length == 0)
 			return message.reply(getLang("noAdmin"));
 		const senderName = await usersData.getName(senderID);
-		const msg = "==📨️𝙲𝙰𝙻𝙻📬𝙴𝙿𝙷𝙴𝙽𝙸📨️=="
+		const msg = "==📨️𝘾𝘼𝙇𝙇📬𝙎𝙊𝙉𝙄𝘾📨️=="
 			+ `\n- User Name: 🌿${senderName}🌿`
-			+ `\n- User ID: 🎀${senderID}🎀`
+			+ `\n- User ID: ★${senderID}★`
 			+ (isGroup ? getLang("sendByGroup", (await threadsData.get(threadID)).threadName, threadID) : getLang("sendByUser"));
 
 		const formMessage = {
